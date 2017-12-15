4e21a58f99b6d30cdfccec96737c0e89205208b5
Ajmal Nabil <ajmalnabiljamal@gmail.com>
2017-12-15 13:00:29 +0800

    Created model Article, and migrated to the database, and edited show, and created actions and templates to blog display, listed all articles, and added links to pages, and added validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,36 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def create
+    # 1st step: render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:text)
   end
 
 end
